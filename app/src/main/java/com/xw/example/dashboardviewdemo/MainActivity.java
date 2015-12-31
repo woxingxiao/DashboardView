130181da2091e3325577409290e0a8e99a638520
woxingxiao <185578499@qq.com>
2015-12-31 14:53:26 +0800

    optimized

--- app/src/main/java/com/xw/example/dashboardviewdemo/MainActivity.java
+++ app/src/main/java/com/xw/example/dashboardviewdemo/MainActivity.java
@@ -20,49 +20,49 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        final DashboardView dashboardView1 = (DashboardView) findViewById(R.id.dashboardView1);
-        DashboardView dashboardView2 = (DashboardView) findViewById(R.id.dashboardView2);
-        DashboardView dashboardView3 = (DashboardView) findViewById(R.id.dashboardView3);
-        dashboardView1.setOnClickListener(new View.OnClickListener() {
+        final DashboardView dashboardView2 = (DashboardView) findViewById(R.id.dashboard_view_2);
+        DashboardView dashboardView3 = (DashboardView) findViewById(R.id.dashboard_view_3);
+        DashboardView dashboardView4 = (DashboardView) findViewById(R.id.dashboard_view_4);
+        dashboardView2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                dashboardView1.setRealTimeValueWithAnim(150.f);
+                dashboardView2.setRealTimeValueWithAnim(150.f);
             }
         });
 
         List<HighlightCR> highlight1 = new ArrayList<>();
         highlight1.add(new HighlightCR(210, 60, Color.parseColor("#03A9F4")));
         highlight1.add(new HighlightCR(270, 60, Color.parseColor("#FFA000")));
-        dashboardView1.setStripeHighlightColorAndRange(highlight1);
+        dashboardView2.setStripeHighlightColorAndRange(highlight1);
 
         List<HighlightCR> highlight2 = new ArrayList<>();
         highlight2.add(new HighlightCR(170, 140, Color.parseColor("#607D8B")));
         highlight2.add(new HighlightCR(310, 60, Color.parseColor("#795548")));
-        dashboardView2.setStripeHighlightColorAndRange(highlight2);
+        dashboardView3.setStripeHighlightColorAndRange(highlight2);
 
-        dashboardView3.setRadius(110);
-        dashboardView3.setArcColor(getResources().getColor(android.R.color.black));
-        dashboardView3.setTextColor(Color.parseColor("#212121"));
-        dashboardView3.setBgColor(getResources().getColor(android.R.color.white));
-        dashboardView3.setStartAngle(150);
-        dashboardView3.setPointerRadius(80);
-        dashboardView3.setCircleRadius(8);
-        dashboardView3.setSweepAngle(240);
-        dashboardView3.setBigSliceCount(12);
-        dashboardView3.setSliceCountInOneBigSlice(2);
-        dashboardView3.setMaxValue(240);
-        dashboardView3.setRealTimeValue(80);
-        dashboardView3.setMeasureTextSize(14);
-        dashboardView3.setHeaderRadius(50);
-        dashboardView3.setHeaderTitle("km/h");
-        dashboardView3.setHeaderTextSize(16);
-        dashboardView3.setStripeWidth(20);
-        dashboardView3.setStripeMode(DashboardView.StripeMode.OUTER);
+        dashboardView4.setRadius(110);
+        dashboardView4.setArcColor(getResources().getColor(android.R.color.black));
+        dashboardView4.setTextColor(Color.parseColor("#212121"));
+        dashboardView4.setBgColor(getResources().getColor(android.R.color.white));
+        dashboardView4.setStartAngle(150);
+        dashboardView4.setPointerRadius(80);
+        dashboardView4.setCircleRadius(8);
+        dashboardView4.setSweepAngle(240);
+        dashboardView4.setBigSliceCount(12);
+        dashboardView4.setSliceCountInOneBigSlice(2);
+        dashboardView4.setMaxValue(240);
+        dashboardView4.setRealTimeValue(80);
+        dashboardView4.setMeasureTextSize(14);
+        dashboardView4.setHeaderRadius(50);
+        dashboardView4.setHeaderTitle("km/h");
+        dashboardView4.setHeaderTextSize(16);
+        dashboardView4.setStripeWidth(20);
+        dashboardView4.setStripeMode(DashboardView.StripeMode.OUTER);
         List<HighlightCR> highlight3 = new ArrayList<>();
         highlight3.add(new HighlightCR(150, 100, Color.parseColor("#4CAF50")));
         highlight3.add(new HighlightCR(250, 80, Color.parseColor("#FFEB3B")));
         highlight3.add(new HighlightCR(330, 60, Color.parseColor("#F44336")));
-        dashboardView3.setStripeHighlightColorAndRange(highlight3);
+        dashboardView4.setStripeHighlightColorAndRange(highlight3);
 
     }
 
